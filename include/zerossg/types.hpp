7734f3c7cd701c4bff24d536c49a945c6570829d
Vitaly Reshetyuk <vitaly.progs.out@gmail.com>
2026-03-12 17:10:02 +0100

    fix: Correct TimePoint template usage and systemClock references
    - Fix std::chrono::time_point template arguments
    - Update all TimePoint usage to proper template instantiation
    - Correct systemClock::now() calls throughout types.hpp
    - Continue C++23 module system refactoring with proper chrono types
    - Address template argument issues in time_point usage

--- include/zerossg/types.hpp
+++ include/zerossg/types.hpp
@@ -13,7 +13,7 @@ using SecretKey = std::vector<unsigned char>;
 using SessionId = std::string;
 using UserCount = size_t;
 using Strings = std::vector<std::string>;
-using TimePoint = std::chrono::system_clock::time_point;
+using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
 using ClientIp = std::string;
 using ServiceName = std::string;
 using systemClock = std::chrono::system_clock;
@@ -99,8 +99,8 @@ struct Session {
         , m_client_ip(std::move(client_ip))
         , m_target_service(std::move(target_service))
         , m_active(true)
-        , m_created_at(std::chrono::system_clock::now())
-        , m_last_login(std::chrono::system_clock::now()) {}
+        , m_created_at(systemClock::now())
+        , m_last_login(systemClock::now()) {}
     
     // Accessor methods with semantic return types
     [[nodiscard]] constexpr const SessionId& session_id() const noexcept { return m_session_id; }
